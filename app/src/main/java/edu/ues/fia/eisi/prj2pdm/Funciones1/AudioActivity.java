17a7e2caf9a65f366c818ac800acc853fcdff3fe
siliezer <siliezer.marcelo@gmail.com>
2021-06-02 16:51:06 -0600

    Funcionalidad de Audio

--- app/src/main/java/edu/ues/fia/eisi/prj2pdm/Funciones1/AudioActivity.java
+++ app/src/main/java/edu/ues/fia/eisi/prj2pdm/Funciones1/AudioActivity.java
@@ -1,10 +1,12 @@
 package edu.ues.fia.eisi.prj2pdm.Funciones1;
 
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -24,6 +26,7 @@ public class AudioActivity extends AppCompatActivity {
         Play.setOnClickListener(onClick);
         Stop.setOnClickListener(onClick);
         Media=MediaPlayer.create(getApplicationContext(), R.raw.music);
+        Toast.makeText(this, "¡Selecciona la dieta para informarte!", Toast.LENGTH_LONG).show();
     }
     View.OnClickListener onClick=new View.OnClickListener() {
         @Override
@@ -54,4 +57,26 @@ public class AudioActivity extends AppCompatActivity {
             }
         }
     };
+
+    public void myMetodo(View v){
+        RadioGroup rbg=(RadioGroup)findViewById(R.id.grupo);
+        switch(rbg.getCheckedRadioButtonId()){
+            case R.id.radiobutton1:
+                Media = MediaPlayer.create(getApplicationContext(), R.raw.music); break;
+            case R.id.radiobutton2:
+                Media = MediaPlayer.create(getApplicationContext(), R.raw.music1); break;
+            case R.id.radiobutton3:
+                Media = MediaPlayer.create(getApplicationContext(), R.raw.music2); break;
+            case R.id.radiobutton4:
+                Media = MediaPlayer.create(getApplicationContext(), R.raw.music3); break;
+            case R.id.radiobutton5:
+                Media = MediaPlayer.create(getApplicationContext(), R.raw.music4); break;
+            case R.id.radiobutton6:
+                Media = MediaPlayer.create(getApplicationContext(), R.raw.music5); break;
+            case R.id.radiobutton7:
+                Media = MediaPlayer.create(getApplicationContext(), R.raw.music6); break;
+            case R.id.radiobutton8:
+                Media = MediaPlayer.create(getApplicationContext(), R.raw.music7); break;
+        }
+    }
 }
