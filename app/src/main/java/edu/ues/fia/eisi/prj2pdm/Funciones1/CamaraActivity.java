62caf5aaaec9f0b6515ead836edec458c40b7027
siliezer <siliezer.marcelo@gmail.com>
2021-06-02 18:54:40 -0600

    Función de cámara

--- app/src/main/java/edu/ues/fia/eisi/prj2pdm/Funciones1/CamaraActivity.java
+++ app/src/main/java/edu/ues/fia/eisi/prj2pdm/Funciones1/CamaraActivity.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import java.io.File;
 import java.util.Calendar;
 
@@ -20,11 +23,14 @@ public class CamaraActivity extends Activity {
     ImageView image;
     final int FOTOGRAFIA = 654;
     Uri file;
-    @Override
+    TextView text;
 
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camara);
+        text = (TextView) findViewById(R.id.textView5);
         TomarFoto = (Button) findViewById(R.id.mainbttomarfoto);
         image = (ImageView) findViewById(R.id.mainimage);
         TomarFoto.setOnClickListener(onClick);
@@ -45,23 +51,28 @@ public class CamaraActivity extends Activity {
     View.OnClickListener onClick=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-// TODO Auto-generated method stub
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder(); //Esto no se debería de usar
+            StrictMode.setVmPolicy(builder.build());
+            // TODO Auto-generated method stub
             Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            File photo =new
-                    File(Environment.getExternalStorageDirectory(),String.valueOf(Calendar.getInstance().getTimeInMillis())+".jpg");
+            File photo =new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/DCIM/",
+                    "Progreso-"+Calendar.getInstance().getTime()+".jpg");
             file=Uri.fromFile(photo);
             intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
             startActivityForResult(intent,FOTOGRAFIA);
         }
     };
+
     @Override
     public void onActivityResult(int RequestCode, int ResultCode, Intent intent) {
         if (RequestCode==FOTOGRAFIA){
             if(ResultCode == RESULT_OK){
+
                 image.setImageURI(file);
+                text.setText("Foto: "+file.getLastPathSegment());
             }
             else{
-                Toast.makeText(getApplicationContext(),"fotografia No tomada",
+                Toast.makeText(getApplicationContext(), "fotografia No tomada",
                         Toast.LENGTH_SHORT).show();
             }
         }
