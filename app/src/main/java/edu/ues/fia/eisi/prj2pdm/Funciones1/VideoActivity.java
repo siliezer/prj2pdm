4ac18f5ccc72d58725f772768278141cb09c1525
Coralinav <mcna97@gmail.com>
2021-06-06 16:39:59 -0600

    agregando generador de qr y unos edits en el html de dieta

--- app/src/main/java/edu/ues/fia/eisi/prj2pdm/Funciones1/VideoActivity.java
+++ app/src/main/java/edu/ues/fia/eisi/prj2pdm/Funciones1/VideoActivity.java
@@ -1,39 +1,93 @@
 package edu.ues.fia.eisi.prj2pdm.Funciones1;
 
-import android.net.Uri;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.zxing.WriterException;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 import java.io.File;
 
 import edu.ues.fia.eisi.prj2pdm.R;
 
 public class VideoActivity extends AppCompatActivity {
-
-    Button Play;
-    Button Stop;
-    VideoView video;
-    MediaController mediacontrol;
+    // variables for imageview, edittext,
+    // button, bitmap and qrencoder.
+    private ImageView qrCodeIV;
+    private EditText dataEdt;
+    private Button generateQrBtn;
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        video=(VideoView) findViewById(R.id.video);
-        File f=new File(Environment.getExternalStorageDirectory().getAbsolutePath() +
-                "/Download/","video.mp4");
-        if (f.exists()){
-            Uri uri= Uri.fromFile(f);
-            video.setVideoURI(uri);
-            mediacontrol=new MediaController(this);
-            video.setMediaController(mediacontrol);
-            mediacontrol.show();
 
-        }
+        // initializing all variables.
+        qrCodeIV = findViewById(R.id.idIVQrcode);
+        dataEdt = findViewById(R.id.idEdt);
+        generateQrBtn = findViewById(R.id.idBtnGenerateQR);
 
+        // initializing onclick listener for button.
+        generateQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(dataEdt.getText().toString())) {
+
+                    // if the edittext inputs are empty then execute
+                    // this method showing a toast message.
+                    Toast.makeText(VideoActivity.this, "Enter some text to generate QR Code", Toast.LENGTH_SHORT).show();
+                } else {
+                    // below line is for getting
+                    // the windowmanager service.
+                    WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+                    // initializing a variable for default display.
+                    Display display = manager.getDefaultDisplay();
+
+                    // creating a variable for point which
+                    // is to be displayed in QR Code.
+                    Point point = new Point();
+                    display.getSize(point);
+
+                    // getting width and
+                    // height of a point
+                    int width = point.x;
+                    int height = point.y;
+
+                    // generating dimension from width and height.
+                    int dimen = width < height ? width : height;
+                    dimen = dimen * 3 / 4;
+
+                    // setting this dimensions inside our qr code
+                    // encoder to generate our qr code.
+                    qrgEncoder = new QRGEncoder(dataEdt.getText().toString(), null, QRGContents.Type.TEXT, dimen);
+                    try {
+                        // getting our qrcode in the form of bitmap.
+                        bitmap = qrgEncoder.encodeAsBitmap();
+                        // the bitmap is set inside our image
+                        // view using .setimagebitmap method.
+                        qrCodeIV.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        // this method is called for
+                        // exception handling.
+                        Log.e("Tag", e.toString());
+                    }
+                }
+            }
+        });
     }
 }
