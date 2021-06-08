e6b3053a36c6b8a93dfde10e54d94e5abd65867b
Coralinav <mcna97@gmail.com>
2021-06-07 19:45:39 -0600

    agregando generador de qr en text to speech

--- app/src/main/java/edu/ues/fia/eisi/prj2pdm/Funciones1/TextToSpeechActivity.java
+++ app/src/main/java/edu/ues/fia/eisi/prj2pdm/Funciones1/TextToSpeechActivity.java
@@ -1,98 +1,93 @@
 package edu.ues.fia.eisi.prj2pdm.Funciones1;
 
-import java.util.HashMap;
-import java.util.Locale;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Environment;
-import android.speech.tts.TextToSpeech;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+import com.google.zxing.WriterException;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+import java.io.File;
 
 import edu.ues.fia.eisi.prj2pdm.R;
 
 public class TextToSpeechActivity extends AppCompatActivity {
-    TextToSpeech tts;
-    TextView Texto;
-    Button BtnPlay;
-    Button BtnSave;
-    private static final String LOG_TAG_EXTERNAL_STORAGE = "EXTERNAL_STORAGE";
-    private static final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE_PERMISSION = 1;
+    // variables for imageview, edittext,
+    // button, bitmap and qrencoder.
+    private ImageView qrCodeIV;
+    private EditText dataEdt;
+    private Button generateQrBtn;
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_to_speech);
-        Texto = (TextView) findViewById(R.id.edtText2Speech);
-        BtnPlay = (Button) findViewById(R.id.btnText2SpeechPlay);
-        BtnSave = (Button) findViewById(R.id.btnText2SpeechSave);
-        // Check whether this app has write external storage permission or not.
-        int writeExternalStoragePermission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-// If do not grant write external storage permission.
-        if (writeExternalStoragePermission != PackageManager.PERMISSION_GRANTED) {
-            // Request user to grant write external storage permission.
-            ActivityCompat.requestPermissions(this, new
-                            String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    REQUEST_CODE_WRITE_EXTERNAL_STORAGE_PERMISSION);
-        }
-        tts = new TextToSpeech(this, OnInit);
-        BtnPlay.setOnClickListener(onClick);
-        BtnSave.setOnClickListener(onClick);
-    }
 
-    TextToSpeech.OnInitListener OnInit = new TextToSpeech.OnInitListener() {
-        @Override
-        public void onInit(int status) {
-            // TODO Auto-generated method stub
-            if (TextToSpeech.SUCCESS == status) {
-                tts.setLanguage(new Locale("spa", "ESP"));
-            } else {
-                Toast.makeText(getApplicationContext(), "TTS no disponible",
-                        Toast.LENGTH_SHORT).show();
-            }
-        }
-    };
-    View.OnClickListener onClick = new View.OnClickListener() {
-        @RequiresApi(api = Build.VERSION_CODES.DONUT)
-        @SuppressLint("SdCardPath")
-        public void onClick(View v) {
-            // TODO Auto-generated method stub
-            if (v.getId() == R.id.btnText2SpeechPlay) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    tts.speak(Texto.getText().toString(), TextToSpeech.QUEUE_FLUSH, null, null);
+        // initializing all variables.
+        qrCodeIV = findViewById(R.id.idIVQrcode);
+        dataEdt = findViewById(R.id.idEdt);
+        generateQrBtn = findViewById(R.id.idBtnGenerateQR);
+
+        // initializing onclick listener for button.
+        generateQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(dataEdt.getText().toString())) {
+
+                    // if the edittext inputs are empty then execute
+                    // this method showing a toast message.
+                    Toast.makeText(TextToSpeechActivity.this, "Enter some text to generate QR Code", Toast.LENGTH_SHORT).show();
                 } else {
-                    tts.speak(Texto.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+                    // below line is for getting
+                    // the windowmanager service.
+                    WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+                    // initializing a variable for default display.
+                    Display display = manager.getDefaultDisplay();
+
+                    // creating a variable for point which
+                    // is to be displayed in QR Code.
+                    Point point = new Point();
+                    display.getSize(point);
+
+                    // getting width and
+                    // height of a point
+                    int width = point.x;
+                    int height = point.y;
+
+                    // generating dimension from width and height.
+                    int dimen = width < height ? width : height;
+                    dimen = dimen * 3 / 4;
+
+                    // setting this dimensions inside our qr code
+                    // encoder to generate our qr code.
+                    qrgEncoder = new QRGEncoder(dataEdt.getText().toString(), null, QRGContents.Type.TEXT, dimen);
+                    try {
+                        // getting our qrcode in the form of bitmap.
+                        bitmap = qrgEncoder.encodeAsBitmap();
+                        // the bitmap is set inside our image
+                        // view using .setimagebitmap method.
+                        qrCodeIV.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        // this method is called for
+                        // exception handling.
+                        Log.e("Tag", e.toString());
+                    }
                 }
             }
-            if (v.getId() == R.id.btnText2SpeechSave) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    tts.speak(Texto.getText().toString(), TextToSpeech.QUEUE_FLUSH, null, null);
-                } else {
-                    tts.speak(Texto.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
-                }
-                HashMap<String, String> myHashRender = new HashMap<String, String>();
-                String Texto_tts = Texto.getText().toString();
-                String destFileName = Environment.getExternalStorageDirectory() + "/tts1.wav";
-                myHashRender.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, Texto_tts);
-                tts.synthesizeToFile(Texto_tts, myHashRender, destFileName);
-            }
-        }
-    };
-
-    public void onDestroy() {
-        tts.shutdown();
-        super.onDestroy();
+        });
     }
 }
-
